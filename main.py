8160fe3b5dad4913dc8745b8cd31e397423c7209
AhmedGebril <40351175+AhmedGebril@users.noreply.github.com>
2022-12-25 21:39:52 +0200

    added comments to help reading the code

--- main.py
+++ main.py
@@ -16,31 +16,31 @@ driver = webdriver.Chrome(service=ser, options=op)
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-
+# gets the items_id and items prices
 def get_store():
     store = driver.find_elements(By.CSS_SELECTOR, '#store b')
     items_id = driver.find_elements(By.CSS_SELECTOR, '#store div')
     return store, items_id
 
 
+# puts the price the items id in a dict
 def get_upgrades():
     upgrades = {}
     for n in range(len(price)):
         upgrades[price[n]] = ids[n]
     return upgrades
 
-
+# automatically updates the price list
 def get_price_list():
     price_list = []
     for item in store:
         element_text = item.text
-        print(element_text)
         if element_text != "":
             prices = element_text.split('-')[1].strip().replace(',', '')
             price_list.append(prices)
     return price_list
 
-
+# gets the id of the items in the store
 def get_ids():
     ids = []
     for id in items_id:
@@ -52,6 +52,8 @@ def get_ids():
 
 dont_stop = True
 while dont_stop:
+
+    # you have to update the page every time you buy an item so you dont get document error
     store, items_id = get_store()
     price = get_price_list()
     ids = get_ids()
@@ -64,10 +66,8 @@ while dont_stop:
             get_upgrade = driver.find_element(By.CSS_SELECTOR, f'#store #{cookie_upgrades[key]}')
             get_upgrade.click()
             print(f'bought {cookie_upgrades[key]}')
-            time.sleep(2)
+            time.sleep(1)
         else:
             break
-
-    print(price)
 
 driver.quit()
